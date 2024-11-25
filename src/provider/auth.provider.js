aad66dd59388bdac98c522b1bbc16ea9c96b1828
crno <118383162+Crn0@users.noreply.github.com.>
2024-11-25 16:19:04 +0800

    build: create auth provider

---
+++ src/provider/auth.provider.js
@@ -0,0 +1,52 @@
+import APIError from '../errors/api.error';
+
+export default class AuthProvider {
+  static token = null;
+
+  static user = null;
+
+  get user() {
+    return this.user;
+  }
+
+  set user(val) {
+    this.user = val;
+  }
+
+  get token() {
+    return this.token;
+  }
+
+  set token(val) {
+    this.token = val;
+  }
+
+  static async refresh() {
+    try {
+      const myHeaders = new Headers();
+
+      myHeaders.append('Content-Type', 'application/json');
+
+      const res = await fetch(
+        `${import.meta.env.VITE_SERVER_URL}/api/v1/auth/tokens/refresh-token`,
+        {
+          method: 'POST',
+          headers: myHeaders,
+          credentials: 'include',
+        },
+      );
+
+      const data = await res.json();
+
+      if (data?.code >= 400) {
+        throw new APIError(data.message, data.code);
+      }
+
+      return [null, data];
+    } catch (error) {
+      if (error instanceof APIError) return [error, null];
+
+      throw error;
+    }
+  }
+}
