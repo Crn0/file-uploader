a0045332ecba65a631402046fdb75f1f44b147c1
crno <118383162+Crn0@users.noreply.github.com.>
2024-11-25 16:17:42 +0800

    build: change route to array from component

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Router from './routes';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import routes from './routes';
 import './global-styles/index.css';
 import './global-styles/reset.css';
-import './global-styles/theme.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router />
-  </React.StrictMode>,
+const router = createBrowserRouter(routes);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
 );
