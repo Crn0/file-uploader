70c0d4f1f96c719dfed2b891d023915b4dfb8080
crno <118383162+Crn0@users.noreply.github.com.>
2024-11-29 13:55:40 +0800

    build: put app in its own directory

---
+++ src/pages/app/app.loader.js
@@ -0,0 +1,31 @@
+import { replace } from 'react-router-dom';
+import AuthProvider from '../../provider/auth.provider';
+import AppService from './app.service';
+import ApiRequest from '../../api/apiRequest';
+
+const BASE_URL = import.meta.env.VITE_SERVER_URL;
+
+const client = new ApiRequest(BASE_URL, AuthProvider);
+
+const service = AppService(client);
+
+const getUser = async () => {
+  const user = service.getUser();
+
+  if (!user) return replace('/re-auth');
+
+  return user;
+};
+
+const silentLogin = async () => {
+  if (AuthProvider.user) return AuthProvider.user;
+
+  const data = service.getUser();
+
+  return { data };
+};
+
+export default {
+  getUser,
+  silentLogin,
+};
