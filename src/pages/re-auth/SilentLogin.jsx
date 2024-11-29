ebcdf2347fb422e6b353c1357a25ab4bc0c7f472
crno <118383162+Crn0@users.noreply.github.com.>
2024-11-29 14:04:40 +0800

    build: remove uselocation hook

--- src/pages/re-auth/SilentLogin.jsx
+++ src/pages/re-auth/SilentLogin.jsx
@@ -1,13 +1,10 @@
 import { useEffect } from 'react';
-import { useNavigate, useAsyncValue, useLocation } from 'react-router-dom';
+import { useNavigate, useAsyncValue } from 'react-router-dom';
 import AuthProvider from '../../provider/auth.provider';
 
 export default function SilentLogin() {
   const [error, data] = useAsyncValue();
   const navigate = useNavigate();
-  const location = useLocation();
-
-  console.log(location);
 
   useEffect(() => {
     if (error) navigate('/login', { replace: true });
