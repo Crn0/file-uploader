dc20f86cae7c2f42e973f9ad8debad294de878cd
crno <118383162+Crn0@users.noreply.github.com.>
2024-12-04 11:41:28 +0800

    build: auth routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,26 +1,44 @@
+import { replace } from 'react-router-dom';
 import App from '../pages/app';
 import Register from '../pages/register';
 import Login from '../pages/login';
+import ErrorHandler from '../components/ui/error';
 import loaders from '../loaders';
 import actions from '../actions';
 
-const { registerAction, loginAction } = actions;
+const { registerAction, loginAction, appAction } = actions;
+const { appLoader, registerLoader, loaginLoader } = loaders;
 
 const routes = [
   {
     path: '/',
-    loader: loaders.appLoader.getUser,
+    loader: appLoader.getUser,
+    action: appAction,
+    shouldRevalidate: () => false,
     element: <App />,
+    errorElement: <ErrorHandler />,
   },
   {
     path: '/register',
+    loader: registerLoader.checkAuth,
     action: registerAction.register,
+    shouldRevalidate: () => false,
     element: <Register />,
+    errorElement: <ErrorHandler />,
   },
   {
     path: '/login',
+    loader: loaginLoader.checkAuth,
     action: loginAction.login,
+    shouldRevalidate: () => false,
     element: <Login />,
+    errorElement: <ErrorHandler />,
+  },
+
+  {
+    path: '*',
+    action: () => replace('/'),
+    element: <ErrorHandler />,
   },
 ];
 
