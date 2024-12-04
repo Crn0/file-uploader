92bb8889794eba867e926f72fd87cce165c05b4b
crno <118383162+Crn0@users.noreply.github.com.>
2024-12-04 11:44:00 +0800

    build: import router actions

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+import registerAction from '../pages/register/register.action';
+import loginAction from '../pages/login/login.action';
+import appAction from '../pages/app/app.action';
+
+export default {
+  registerAction,
+  loginAction,
+  appAction,
+};
