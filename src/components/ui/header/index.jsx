701f5fba9b3261c38be1c4c546f8f86c80017888
crno <118383162+Crn0@users.noreply.github.com.>
2024-12-18 18:14:42 +0800

    style: add style to header

--- src/components/ui/header/index.jsx
+++ src/components/ui/header/index.jsx
@@ -4,16 +4,16 @@ import style from './css/index.module.css';
 
 export default function Header() {
   return (
-    <header>
+    <header className={`${style.header} ${style['pad-1rem']} ${style['header--dark']}`}>
       <div className={`${style.header__left}`}>
-        <div className={`${style.header__btn} ${style.center}}`}>
-          <Link to='/'>File Uploader</Link>
+        <div className={`${style.header__btn} ${style.center} ${style['btn--dark']}`}>
+          <Link customStyles={`${style.link} ${style['link--dark']}`} to='/'>
+            File Uploader
+          </Link>
         </div>
       </div>
 
-      <div className={`${style.header__right}`}>
-        <NavBar />
-      </div>
+      <NavBar customStyles={`${style.header__right}`} />
     </header>
   );
 }
