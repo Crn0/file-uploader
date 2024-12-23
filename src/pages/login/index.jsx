3450db3e260d7f3805d89c1a9a71be3ccf64cdfe
crno <118383162+Crn0@users.noreply.github.com.>
2024-12-23 16:21:19 +0800

    style: add style to login page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -7,10 +7,12 @@ import {
   useNavigate,
   useSearchParams,
 } from 'react-router-dom';
+import Header from '../../components/ui/header';
 import AuthProvider from '../../provider/auth.provider';
 import LoginForm from './LoginForm';
 import Link from '../../components/ui/link';
 import Spinner from '../../components/ui/spinner';
+import styles from './css/index.module.css';
 
 function Wrapper() {
   const actionData = useActionData();
@@ -31,38 +33,49 @@ function Wrapper() {
   }, [asyncValue, navigate, searchParams]);
 
   return (
-    <main>
-      <section>
-        <div>
-          <h1>Login</h1>
-          <p>
-            Don&apos;t have an account yet? <Link to='/register'>Register</Link>
-          </p>
+    <div className={`${styles.app}`}>
+      <Header />
+      <main className={`${styles.grid} ${styles.grid_center}`}>
+        <div
+          className={`${styles.grid} ${styles.grid_center} ${styles.border_white}  ${styles.margin_top_5rem} ${styles.pad_4rem} ${styles.gap_1rem}`}
+        >
+          <div className={`${styles.grid} ${styles.grid_center} `}>
+            <h1>Login</h1>
+            <p>
+              Don&apos;t have an account yet?{' '}
+              <Link to='/register' customStyles={`${styles.link_redirect}`}>
+                Register
+              </Link>
+            </p>
+          </div>
+
+          {actionData && (
+            <div>
+              {((data) => {
+                if (!data) return null;
+
+                if (Array.isArray(data?.errors) && data.errors.length !== 0) {
+                  return (
+                    <>
+                      {data.errors.map((e) => (
+                        <p key={e.message} style={{ color: 'red' }}>
+                          {e.message}
+                        </p>
+                      ))}
+                    </>
+                  );
+                }
+                return <p style={{ color: 'red' }}>{data?.message || data}</p>;
+              })(actionData)}
+            </div>
+          )}
+
+          <div>
+            <LoginForm />
+          </div>
         </div>
-
-        <div>
-          {((data) => {
-            if (Array.isArray(data?.errors)) {
-              return (
-                <>
-                  {data.errors.map((e) => (
-                    <p key={e.message}>{e.message}</p>
-                  ))}
-                </>
-              );
-            }
-
-            if (data?.message) return <p>{data.message}</p>;
-
-            return null;
-          })(actionData)}
-        </div>
-
-        <div>
-          <LoginForm />
-        </div>
-      </section>
-    </main>
+      </main>
+    </div>
   );
 }
 
