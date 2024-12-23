05af86facd929e3534ada9b14a41c081df70037a
crno <118383162+Crn0@users.noreply.github.com.>
2024-12-23 16:36:17 +0800

    style: add style to register page

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,29 +1,92 @@
-import { useActionData } from 'react-router-dom';
-import Link from '../../components/ui/link';
+import { Suspense, useEffect } from 'react';
+import {
+  Await,
+  useActionData,
+  useAsyncValue,
+  useLoaderData,
+  useNavigate,
+  useSearchParams,
+} from 'react-router-dom';
+import AuthProvider from '../../provider/auth.provider';
+import Header from '../../components/ui/header';
 import RegisterForm from './RegisterForm';
+import Spinner from '../../components/ui/spinner';
+import Link from '../../components/ui/link';
+import styles from './css/index.module.css';
 
-export default function Register() {
+function Wrapper() {
   const actionData = useActionData();
+  const asyncValue = useAsyncValue();
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    if (asyncValue[1]) {
+      const { user } = asyncValue[1];
+
+      AuthProvider.user = user;
+
+      const from = searchParams.get('from') || '/';
+
+      navigate(from, { replace: true });
+    }
+  }, [asyncValue, navigate, searchParams]);
 
   return (
-    <main>
-      <section>
-        <div>
-          <h1>Register</h1>
-          <p>
-            Already have an account? <Link to='/login'>Login</Link>
-          </p>
-        </div>
+    <div>
+      <Header />
+      <main className={`${styles.grid} ${styles.grid_center}`}>
+        <div
+          className={`${styles.grid} ${styles.grid_center} ${styles.border_white}  ${styles.margin_top_5rem} ${styles.pad_4rem} ${styles.gap_1rem}`}
+        >
+          <div className={`${styles.grid} ${styles.grid_center} `}>
+            <h1>Register</h1>
+            <p>
+              Already have an account?{' '}
+              <Link to='/login' customStyles={`${styles.link_redirect}`}>
+                Login
+              </Link>
+            </p>
+          </div>
 
-        <div>
-          {actionData &&
-            actionData?.errors?.map((error) => <p key={error.message}>{error.message}</p>)}
-        </div>
+          {actionData && (
+            <div>
+              {((data) => {
+                if (!data) return null;
 
-        <div>
-          <RegisterForm />
+                if (Array.isArray(data?.errors) && data.errors.length !== 0) {
+                  return (
+                    <>
+                      {data.errors.map((e) => (
+                        <p key={e.message} style={{ color: 'red' }}>
+                          {e.message}
+                        </p>
+                      ))}
+                    </>
+                  );
+                }
+                return <p style={{ color: 'red' }}>{data?.message || data}</p>;
+              })(actionData)}
+            </div>
+          )}
+
+          <div>
+            <RegisterForm />
+          </div>
         </div>
-      </section>
-    </main>
+      </main>
+    </div>
+  );
+}
+
+export default function Register() {
+  const loaderData = useLoaderData();
+
+  return (
+    <Suspense fallback={<Spinner />}>
+      <Await resolve={loaderData.data}>
+        <Wrapper />
+      </Await>
+    </Suspense>
   );
 }
