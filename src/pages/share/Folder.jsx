85facf83c4288aa73296a7cf448e63f54a1b7d45
crno <118383162+Crn0@users.noreply.github.com.>
2024-12-23 14:50:11 +0800

    style: add style to share page

--- src/pages/share/Folder.jsx
+++ src/pages/share/Folder.jsx
@@ -1,25 +1,44 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import styles from './css/folder.module.css';
 
-export default function FolderComponent({ folder, url }) {
+dayjs.extend(relativeTime);
+
+export default function FolderComponent({ folder }) {
+  const [currentId, setcurrentId] = useState(null);
+
   const navigate = useNavigate();
-  const handleRowClick = () => {
-    const to = `${url}&folderId=${folder.id}&type=sub-folder`;
-    navigate(to);
-  };
-  return (
-    <tr onClick={handleRowClick}>
-      <td>{folder.name}</td>
 
-      <td>{folder.type}</td>
-      <td>—</td>
-      <td>{folder.createdAt}</td>
+  const handleRowClick = () => {
+    navigate(`/folders/${folder.id}`);
+  };
+
+  const onMouseOver = () => setcurrentId(folder.id);
+  const onMouseLeave = () => setcurrentId(null);
+
+  return (
+    <tr
+      className={`${currentId === folder.id ? styles.onHover : ''}`}
+      onClick={handleRowClick}
+      onMouseOver={onMouseOver}
+      onMouseLeave={onMouseLeave}
+      onFocus={onMouseOver}
+    >
+      <td className={`${styles.td}`}>
+        <div className={`${styles.td__name}`}>{folder.name}</div>
+      </td>
+
+      <td className={`${styles.td}`}>{folder.type}</td>
+      <td className={`${styles.td}`}>—</td>
+      <td className={`${styles.td}`}>{dayjs(folder.createdAt).fromNow()}</td>
     </tr>
   );
 }
 
 FolderComponent.propTypes = {
-  url: PropTypes.string.isRequired,
   folder: PropTypes.shape({
     id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
--- src/pages/share/SortHeader.jsx
+++ src/pages/share/SortHeader.jsx
@@ -1,46 +1,44 @@
 import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
-import { useAsyncValue, useFetcher, useLocation } from 'react-router-dom';
+import { useAsyncValue, useFetcher } from 'react-router-dom';
+import { FaCaretUp, FaCaretDown } from 'react-icons/fa';
 import Button from '../../components/ui/button';
 import styles from './css/sort-header.module.css';
 
 export default function SortHeader({ setFolders, setFiles }) {
-  const [
-    _,
-    {
-      data: { folder },
-    },
-  ] = useAsyncValue();
+  const [error, asyncData] = useAsyncValue();
+  const [activeSortIndex, setActivceSortIndex] = useState(0);
+
+  if (error) throw error;
 
   const fetcher = useFetcher();
   const [sort, setSort] = useState('ASC');
-  const location = useLocation();
-  const params = new URLSearchParams(location.search);
+  const folder = asyncData.data?.folder;
 
   const sortName = () => {
     const formData = new FormData();
-    const sortBy = sort === 'ASC' ? 'name' : '-name';
+    const sortBy = sort !== 'ASC' ? 'name' : '-name';
 
-    formData.append('token', params.get('token'));
     formData.append('intent', 'folder:sort');
     formData.append('id', folder.id);
     formData.append('sort', sortBy);
 
-    fetcher.submit(formData, { action: '/share' });
+    fetcher.submit(formData, { action: `/folders/${folder.id}` });
     setSort((prev) => (prev === 'ASC' ? 'DESC' : 'ASC'));
+    setActivceSortIndex(0);
   };
 
   const sortDate = () => {
     const formData = new FormData();
-    const sortBy = sort === 'ASC' ? 'createdAt' : '-createdAt';
+    const sortBy = sort !== 'ASC' ? 'createdAt' : '-createdAt';
 
-    formData.append('token', params.get('token'));
     formData.append('intent', 'folder:sort');
     formData.append('id', folder.id);
     formData.append('sort', sortBy);
 
-    fetcher.submit(formData, { action: '/share' });
+    fetcher.submit(formData, { action: `/folders/${folder.id}` });
     setSort((prev) => (prev === 'ASC' ? 'DESC' : 'ASC'));
+    setActivceSortIndex(1);
   };
 
   useEffect(() => {
@@ -51,29 +49,48 @@ export default function SortHeader({ setFolders, setFiles }) {
       setFiles(data.folder.files);
     }
   }, [fetcher.data, folder.files, folder.folders, setFiles, setFolders]);
-
   return (
     <thead>
       <tr>
-        <th scope='col' id='name'>
-          <Button type='button' na size='lg' onClick={sortName}>
-            Name
+        <th scope='col' id='name' className={`${styles.th}`}>
+          <Button type='button' na size='lg' onClick={sortName} customStyles={`${styles.btn}`}>
+            <div className={`${styles.flex} ${styles.gap_1}`}>
+              Name
+              {(() => {
+                if (activeSortIndex !== 0) return null;
+
+                return sort === 'ASC' ? <FaCaretUp /> : <FaCaretDown />;
+              })()}
+            </div>
           </Button>
         </th>
-        <th scope='col' id='type'>
-          <Button type='button' na size='lg'>
-            Type
+        <th scope='col' id='type' className={`${styles.th}`}>
+          <Button type='button' na size='lg' customStyles={`${styles.btn}`}>
+            <div>Type</div>
           </Button>
         </th>
-        <th scope='col' id='size'>
-          <Button type='button' na size='lg'>
-            Size
+        <th scope='col' id='size' className={`${styles.th}`}>
+          <Button type='button' na size='lg' customStyles={`${styles.btn}`}>
+            <div>Size</div>
           </Button>
         </th>
 
-        <th scope='col' id='creation_date'>
-          <Button type='button' na size='lg' onClick={sortDate}>
-            Creation Date
+        <th scope='col' id='creation_date' className={`${styles.th}`}>
+          <Button
+            type='button'
+            na
+            size='lg'
+            onClick={sortDate}
+            customStyles={`${styles.btn} ${styles.flex} ${styles.flex__center}`}
+          >
+            <div className={`${styles.flex} ${styles.gap_1}`}>
+              Creation Date
+              {(() => {
+                if (activeSortIndex !== 1) return null;
+
+                return sort === 'ASC' ? <FaCaretUp /> : <FaCaretDown />;
+              })()}
+            </div>
           </Button>
         </th>
       </tr>
