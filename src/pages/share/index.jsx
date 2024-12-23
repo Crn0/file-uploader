ccfb6c1c738302d3dd3f3e4e580ef4d0504ab93f
crno <118383162+Crn0@users.noreply.github.com.>
2024-12-23 14:50:11 +0800

    style: add style to share page

--- src/pages/share/index.jsx
+++ src/pages/share/index.jsx
@@ -1,6 +1,10 @@
 import { useState, Suspense, useEffect, useReducer, useMemo } from 'react';
 import { useLoaderData, Await, useFetcher, useAsyncValue, useLocation } from 'react-router-dom';
+import dayjs from 'dayjs';
+import Calendar from 'dayjs/plugin/calendar';
+import helpers from '../../helpers';
 import { reducer, reducerState } from './reducer';
+import UserContext from '../../context/user.context';
 import Header from '../../components/ui/header';
 import SortHeader from './SortHeader';
 import FileComponent from './File';
@@ -10,16 +14,10 @@ import Spinner from '../../components/ui/spinner';
 import Button from '../../components/ui/button';
 import Link from '../../components/ui/link';
 import styles from './css/index.module.css';
-import UserContext from '../../context/user.context';
 
-function deepEqual(x, y) {
-  const ok = Object.keys;
-  const tx = typeof x;
-  const ty = typeof y;
-  return x && y && tx === 'object' && tx === ty
-    ? ok(x).length === ok(y).length && ok(x).every((key) => deepEqual(x[key], y[key]))
-    : x === y;
-}
+dayjs.extend(Calendar);
+
+const { deepEqual, formatBytes } = helpers;
 
 function Wrapper() {
   const [error, asyncData] = useAsyncValue();
@@ -107,55 +105,14 @@ function Wrapper() {
   }, [resourceAction, isLoading, isSubmitting, fetcher.data, fetcherData.prev, asyncData]);
 
   return (
-    <>
+    <div className={`${styles.app}`}>
       <Header />
 
-      <main>
-        <div>
-          {paths?.map((path, index) => {
-            if (index === 0) {
-              return (
-                <div key={path.id}>
-                  <div>
-                    <div>
-                      <Link to={`${location.pathname}?token=${params.get('token')}`}>
-                        <span>{path.name}</span>
-                      </Link>
-                    </div>
-
-                    {index !== paths.length - 1 && (
-                      <div>
-                        <span> {'>'} </span>
-                      </div>
-                    )}
-                  </div>
-                </div>
-              );
-            }
-
-            return (
-              <div key={path.id}>
-                <div>
-                  <Link
-                    key={path.id}
-                    to={`${location.pathname}?token=${params.get('token')}&folderId=${path.id}&type=sub-folder`}
-                  >
-                    {path.name}
-                  </Link>
-
-                  {index !== paths.length - 1 && (
-                    <div>
-                      <span> {'>'} </span>
-                    </div>
-                  )}
-                </div>
-              </div>
-            );
-          })}
-        </div>
-
+      <div className={`${styles.main}`}>
         {resourceAction.file['file:preview'].on && (
-          <div>
+          <div
+            className={`${styles.grid} ${styles.grid__center} ${styles.gap_1} ${styles.preview}`}
+          >
             {(() => {
               const isPreview = resourceAction.file['file:preview'].on;
 
@@ -167,143 +124,237 @@ function Wrapper() {
               if (e) throw e;
 
               return (
-                <div>
+                <>
                   <div>
-                    <div>{apiData?.fileName}</div>
                     <div>
-                      <Button type='button' size='xxs' onClick={closePreview}>
+                      <Button
+                        type='button'
+                        size='xxs'
+                        customStyles={`${styles.modal__btn}`}
+                        testId='btn__preview__close'
+                        onClick={closePreview}
+                      >
                         X
                       </Button>
                     </div>
                   </div>
 
                   {apiData && (
-                    <div style={{ width: '500px' }}>
+                    <div className={`${styles.preview__image}`}>
                       <img src={apiData.url} alt={apiData.fileName} />
                     </div>
                   )}
-                </div>
+                </>
               );
             })()}
           </div>
         )}
-        <table>
-          <SortHeader setFolders={setFolders} setFiles={setFiles} />
-
-          {/* BODY */}
-          <tbody>
-            {(() => {
-              if (folders.length === 0 && files.length === 0)
-                return (
-                  <tr>
-                    <td colSpan='4'>Empty folder</td>
-                  </tr>
-                );
-
-              return (
-                <>
-                  {folders.length !== 0 &&
-                    folders.map((folder) => (
-                      <FolderComponent
-                        key={folder.id}
-                        folder={folder}
-                        url={`${location.pathname}?token=${params.get('token')}`}
-                      />
-                    ))}
-                  {files.length !== 0 &&
-                    files.map((file) => (
-                      <FileComponent key={file.id} file={file} setActiveId={setActiveId} />
-                    ))}
-                </>
-              );
-            })()}
-          </tbody>
-        </table>
 
         <div>
-          {files.length !== 0 &&
-            files.map((file) => (
-              <FileModal
-                key={`${file.name} ${file.id}`}
-                title='File detail'
-                buttonText=':'
-                modalId={file.id}
-                activeId={activeId}
-                setActiveId={setActiveId}
-                done={!resourceActionIsLoading('file:preview', file.id)}
-                on={resourceAction.file['file:preview'].on}
-                hasButton={false}
-                isTable
-              >
-                <div>
-                  {(() => {
-                    const validFields = {
-                      name: 'Name',
-                      size: 'Size',
-                      createdAt: 'Created',
-                      type: 'Type',
-                      extension: 'Extension',
-                    };
+          <div
+            className={`${styles.flex} ${styles.gap_1} ${styles.flex__wrap} ${styles.margin_bottom_2rem}`}
+          >
+            {paths?.map((path, index) => {
+              if (index !== paths.length - 1) {
+                return (
+                  <div
+                    key={path.id}
+                    className={`${styles.flex} ${styles.gap_1} ${styles.flex__wrap}`}
+                  >
+                    <div>
+                      <Link to={`/folders/${path.id}`} customStyles={`${styles.link}`}>
+                        {path.name}
+                      </Link>
+                    </div>
 
-                    const entries = Object.entries(file);
+                    <div>
+                      <span className={`${styles.font_2em}`}> {'>'} </span>
+                    </div>
+                  </div>
+                );
+              }
 
-                    return (
-                      <>
-                        {entries.map(([key, _]) => {
-                          if (!validFields[key]) return null;
+              return (
+                <div
+                  key={path.id}
+                  className={`${styles.flex} ${styles.gap_1} ${styles.flex__wrap}`}
+                >
+                  <Link key={path.id} to={`/folders/${path.id}`} customStyles={`${styles.link}`}>
+                    {path.name}
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
+
+          <table className={`${styles.table}`}>
+            <colgroup>
+              <col span='1' className={`${styles.table__col__name}`} />
+              <col span='1' />
+              <col span='1' />
+              <col span='1' />
+            </colgroup>
+            <SortHeader setFolders={setFolders} setFiles={setFiles} />
+
+            {/* BODY */}
+            <tbody>
+              {(() => {
+                if (folders.length === 0 && files.length === 0)
+                  return (
+                    <tr>
+                      <td colSpan='4' style={{ textAlign: 'center' }}>
+                        Empty folder
+                      </td>
+                    </tr>
+                  );
+
+                return (
+                  <>
+                    {folders.length !== 0 &&
+                      folders.map((folder) => <FolderComponent key={folder.id} folder={folder} />)}
+                    {files.length !== 0 &&
+                      files.map((file) => (
+                        <FileComponent key={file.id} file={file} setActiveId={setActiveId} />
+                      ))}
+                  </>
+                );
+              })()}
+            </tbody>
+          </table>
+        </div>
+
+        {(() => {
+          if (files.length === 0) return null;
+
+          return (
+            <>
+              {files.map((file) => {
+                if (file.id !== activeId) return null;
+
+                return (
+                  <div key={file.id} className={`${styles.file__info__modal__container}`}>
+                    <FileModal
+                      key={`${file.name} ${file.id}`}
+                      title='File detail'
+                      buttonText=':'
+                      modalCustomStyles={`${styles.dialog}  ${styles.file__info__modal}`}
+                      modalCloseButtonCustomStyles={`${styles.modal__btn}`}
+                      modalId={file.id}
+                      activeId={activeId}
+                      setActiveId={setActiveId}
+                      on={resourceAction.file['file:preview'].on}
+                      done={!resourceActionIsLoading('file:preview', file.id)}
+                      hasButton={false}
+                      hasChildModal={resourceAction.file['file:share'].id === file.id}
+                      isTable
+                    >
+                      <div className={`${styles.margin_bottom_hundred_percent}`}>
+                        {(() => {
+                          const validFields = {
+                            name: 'Name',
+                            size: 'Size',
+                            createdAt: 'Created',
+                            type: 'Type',
+                            extension: 'Extension',
+                          };
+
+                          const entries = Object.entries(file);
 
                           return (
-                            <div key={`${file.id} ${key}`}>
-                              <div>
-                                <span>{validFields[key]}</span>
-                                <div>
-                                  <span>{file[key]}</span>
-                                </div>
-                              </div>
+                            <>
+                              {entries.map(([key, _]) => {
+                                if (!validFields[key]) return null;
+
+                                return (
+                                  <div
+                                    key={`${file.id} ${key}`}
+                                    className={styles.margin_bottom_8px}
+                                  >
+                                    <div>
+                                      {(() => {
+                                        if (key === 'size') {
+                                          return (
+                                            <>
+                                              <div className={`${styles.bold}`}>
+                                                {validFields[key]}
+                                              </div>
+                                              <div>{formatBytes(file[key])}</div>
+                                            </>
+                                          );
+                                        }
+
+                                        if (key === 'createdAt') {
+                                          return (
+                                            <>
+                                              <div className={`${styles.bold}`}>
+                                                {validFields[key]}
+                                              </div>
+                                              <div>{dayjs().calendar(dayjs(file[key]))}</div>
+                                            </>
+                                          );
+                                        }
+                                        return (
+                                          <>
+                                            <div className={`${styles.bold}`}>
+                                              {validFields[key]}
+                                            </div>
+                                            <div>{file[key]}</div>
+                                          </>
+                                        );
+                                      })()}
+                                    </div>
+                                  </div>
+                                );
+                              })}
+                            </>
+                          );
+                        })()}
+                      </div>
+
+                      <div className={`${styles.file__info__modal__middle}`}>
+                        {(() => {
+                          if (file.extension === 'epub') return null;
+
+                          return (
+                            <div>
+                              <Button
+                                type='submit'
+                                size='xs'
+                                isLoading={resourceActionIsLoading('file:preview', file.id)}
+                                disabled={resourceActionIsLoading('file:preview', file.id)}
+                                onClick={handleFileAction(file.id, 'file:preview')}
+                                testId='btn__file__preview'
+                                customStyles={`${styles.action__btn}`}
+                              >
+                                Preview
+                              </Button>
                             </div>
                           );
-                        })}
-                      </>
-                    );
-                  })()}
-                </div>
+                        })()}
 
-                {(() => {
-                  if (file.extension === 'epub') return null;
-
-                  return (
-                    <div>
-                      <Button
-                        type='submit'
-                        size='xs'
-                        isLoading={resourceActionIsLoading('file:preview', file.id)}
-                        disabled={resourceActionIsLoading('file:preview', file.id)}
-                        onClick={handleFileAction(file.id, 'file:preview')}
-                        testId='btn__file__preview'
-                      >
-                        Preview
-                      </Button>
-                    </div>
-                  );
-                })()}
-
-                <div>
-                  <Button
-                    type='submit'
-                    size='xs'
-                    isLoading={resourceActionIsLoading('file:download')}
-                    disabled={resourceActionIsLoading('file:download')}
-                    onClick={handleFileAction(file.id, 'file:download')}
-                    testId='btn__file__download'
-                  >
-                    Download
-                  </Button>
-                </div>
-              </FileModal>
-            ))}
-        </div>
-      </main>
-    </>
+                        <div>
+                          <Button
+                            type='submit'
+                            size='xs'
+                            isLoading={resourceActionIsLoading('file:download')}
+                            disabled={resourceActionIsLoading('file:download')}
+                            onClick={handleFileAction(file.id, 'file:download')}
+                            testId='btn__file__download'
+                            customStyles={`${styles.action__btn}`}
+                          >
+                            Download
+                          </Button>
+                        </div>
+                      </div>
+                    </FileModal>
+                  </div>
+                );
+              })}
+            </>
+          );
+        })()}
+      </div>
+    </div>
   );
 }
 
