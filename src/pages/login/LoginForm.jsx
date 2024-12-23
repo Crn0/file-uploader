e2ba1c4ee736bda0403514715165622e669c2004
crno <118383162+Crn0@users.noreply.github.com.>
2024-12-23 16:21:19 +0800

    style: add style to login page

--- src/pages/login/LoginForm.jsx
+++ src/pages/login/LoginForm.jsx
@@ -1,15 +1,14 @@
-import PropTypes from 'prop-types';
 import { useNavigation, useLocation } from 'react-router-dom';
 import Form from '../../components/ui/form';
-import Fieldset from '../../components/ui/form/Fieldset';
 import Label from '../../components/ui/form/Label';
 import Input from '../../components/ui/form/Input';
 import Button from '../../components/ui/button';
-import style from './css/form.module.css';
+import Spinner from '../../components/ui/spinner';
+import styles from './css/form.module.css';
 
 const GOOGLE_URL = `${import.meta.env.VITE_SERVER_URL}/api/v1/auth/google`;
 
-export default function LoginForm({ customStyles = '' }) {
+export default function LoginForm() {
   const location = useLocation();
   const navigation = useNavigation();
 
@@ -17,50 +16,71 @@ export default function LoginForm({ customStyles = '' }) {
   const isSubmitting = navigation.state === 'submitting';
 
   return (
-    <Form action='/login' method='POST' customStyles={customStyles}>
+    <Form action='/login' method='POST' customStyles={`${styles.form}`}>
       <Input type='hidden' name='redirectTo' value={from} autoComplete='off' />
-      <Fieldset id={style.email_pwd__field}>
+      <div>
         <Label name='Email:'>
-          <Input type='email' name='email' autoComplete='off' uncontrolled />
+          <Input
+            type='email'
+            name='email'
+            autoComplete='off'
+            customStyles={`${styles.block}`}
+            uncontrolled
+          />
         </Label>
+      </div>
 
+      <div>
         <Label name='Password:'>
-          <Input type='password' name='password' autoComplete='new-password' uncontrolled />
+          <Input
+            type='password'
+            name='password'
+            autoComplete='new-password'
+            customStyles={`${styles.block}`}
+            uncontrolled
+          />
         </Label>
-      </Fieldset>
+      </div>
 
-      <Fieldset id={style.btn__field}>
+      <div
+        className={`${styles.flex} ${styles.flex_center} ${styles.margin_top_2rem} ${styles.gap_1rem}`}
+      >
         {(() => {
           if (isSubmitting)
             return (
-              <Button
-                type='button'
-                size='m'
-                isLoading={isSubmitting}
-                disabled={isSubmitting}
-                testId='btn_login'
-              >
-                Login
-              </Button>
+              <>
+                <Button
+                  type='button'
+                  size='m'
+                  customStyles={`${styles.button}`}
+                  isLoading={isSubmitting}
+                  disabled={isSubmitting}
+                  testId='btn_login'
+                >
+                  Login
+                </Button>
+                <a
+                  className={`${styles.link_btn} ${isSubmitting ? styles.link_disable : ''}`}
+                  href={GOOGLE_URL}
+                  onClick={(e) => e.preventDefault()}
+                >
+                  {isSubmitting ? <Spinner /> : <span>Google</span>}
+                </a>
+              </>
             );
 
           return (
-            <Button type='submit' size='m' testId='btn_login'>
-              Login
-            </Button>
+            <>
+              <Button type='submit' size='m' customStyles={`${styles.button}`} testId='btn_login'>
+                Login
+              </Button>
+              <a className={`${styles.link_btn}`} href={GOOGLE_URL}>
+                Google
+              </a>
+            </>
           );
         })()}
-      </Fieldset>
-
-      <Fieldset id={style.oauth__field}>
-        <a className={`${style.link__oauth}`} href={GOOGLE_URL}>
-          Or Login with Google
-        </a>
-      </Fieldset>
+      </div>
     </Form>
   );
 }
-
-LoginForm.propTypes = {
-  customStyles: PropTypes.string,
-};
